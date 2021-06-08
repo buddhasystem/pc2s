799320f2b33b0ffe53ad3786cc595032d19ded48
Maxim Potekhin <potekhin@bnl.gov>
2021-06-08 15:30:15 -0400

    Stubs for views and urls for the landing page

--- server/info/views.py
+++ server/info/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'index.html', {'message': 'hi'})
+
+#######################################################
+#     return HttpResponse("Future home of the CDB.")
+
