089950b15646dc275f621867662762139e9af615
Maxim Potekhin <potekhin@bnl.gov>
2021-06-08 18:41:43 -0400

    Tweaks in URLs with regex capture

--- server/ts/views.py
+++ server/ts/views.py
@@ -8,3 +8,6 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'test_index.html', {'message': 'hi'})
 
+def dist(request, t):
+    return render(request, 'test_index.html', {'message': 'dist'+str(t)})
+
