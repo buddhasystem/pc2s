1ce8ee08fbf99244116cbcdf0b1766528c038b59
Maxim Potekhin <potekhin@bnl.gov>
2021-07-14 21:39:10 -0400

    Added a stub for TS, also added correct links to the sidebar.

--- server/cdb/views.py
+++ server/cdb/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from .models import *
 
 def index(request):
-    return render(request, 'cdb.html', {'message': 'cdb'})
+    return render(request, 'cdb.html', {'active': 'cdb'})
 
 #######################################################
 #     return HttpResponse("For debugging only.")
--- server/ts/views.py
+++ server/ts/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
 
 
-
-from django.http import HttpResponse
-
-
 def index(request):
-    return render(request, 'test_index.html', {'message': 'ts'})
+    return render(request, 'ts.html', {'active': 'ts'})
 
 def dist(request, t):
     return render(request, 'test_index.html', {'message': 'dist'+str(t)})
